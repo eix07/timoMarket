ca58b847948bff5e3e00542e5249611081edccb7
Miguel Rivera <Miguel Rivera>
2017-02-12 21:07:12 -0500

    blaa

---
+++ public/js/controller.js
@@ -0,0 +1,72 @@
+angular.module('market',[])
+	.controller('mainCtrl',['itemService','cartService',function(itemService,cartService){
+		var self=this;
+		self.pressed=true;
+		console.log('Controller created beatch');
+		self.carrito=cartService.merca();
+		console.log(self.carrito.lenght);
+		self.loadData=function(){
+			self.pressed=false;
+			self.products=itemService.full();
+		};
+		self.addCart=function(p){
+			cartService.add(p);
+			self.carrito=cartService.merca();
+			console.log(cartService.merca());
+		};
+		self.removeCart=function(p){
+			cartService.remove(p);
+			self.carrito=cartService.merca();
+		}
+	}])
+	.filter('category',[function(){
+		return function(input,cate){
+			var res=[];
+			for(var n in input){
+				var c=input[n].categories;
+				if(c.indexOf(cate)!=-1){
+					res.push(input[n]);
+				}
+			}
+			return res;
+		}
+	}])
+	.filter('bestFilter',[function(){
+		return function(input){
+			var res=[];
+			for(var n in input){
+				var c=input[n].best_seller;
+				if(c){
+					res.push(input[n]);
+				}
+			}
+			return res;
+		}
+	}])
+	.factory('itemService',['$http',function($http){
+		var data=[];
+		$http.get('/getData').then(function(res){
+			data=res.data;
+			console.log('daticos'+res.data);
+		});
+		return{
+			full: function(){
+				return data;
+			}
+		};
+	}])
+	.factory('cartService',[function(){
+		var cart=[];
+		return{
+			add: function(data){
+				cart.push(data);
+			},
+			merca: function(){
+				return cart;
+			},
+			remove: function(p){
+				var index=cart.indexOf(p);
+				cart.splice(index,1);
+			}
+		};
+	}]);
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path=require('path');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/getData',function(req,res){
+  res.sendFile(path.join(__dirname,'../public','dataParcial.json'));
+});
+
+module.exports = router;
